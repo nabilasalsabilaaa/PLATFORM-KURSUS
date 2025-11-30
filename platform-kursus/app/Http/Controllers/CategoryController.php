fe733264f27e4a615392ccf85b420b2c3244d8b8
nabilasalsabilaaa <naabilasalsabiilaa@gmail.com>
2025-11-30 23:28:38 +0800

    ubah order

--- platform-kursus/app/Http/Controllers/CategoryController.php
+++ platform-kursus/app/Http/Controllers/CategoryController.php
@@ -12,7 +12,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::orderBy('name')->paginate(10);
+        $categories = Category::orderBy('id', 'desc')->paginate(10);
 
         return view('categories.index', compact('categories'));
     }
